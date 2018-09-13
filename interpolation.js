beb609b19e034c2a5717fabf21400da0a9189265
Raul Alfonso Larriega Gallegos <rlarriega@esan.edu.pe>
2018-09-13 13:17:06 -0500

    interpolation

---
+++ interpolation.js
@@ -0,0 +1,6 @@
+var persona = { nombre:'kelly', edad:25, apellido:'Baldeon'};
+//ES5
+var texto = "Hola" + persona.nombre + " " + persona.apellido + "tu edad es "+ persona.edad; 
+
+//ES6
+var texto = `Hola ${persona.nombre} ${persona.apellido} tu edad es ${persona.edad}`;
