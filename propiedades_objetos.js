1e870297d7a634e2c37b88f3a2e7e0f5178147f8
Raul Alfonso Larriega Gallegos <rlarriega@esan.edu.pe>
2018-09-14 15:48:48 -0500

    Version abreviada de propiedades de objetos

---
+++ propiedades_objetos.js
@@ -0,0 +1,8 @@
+var nombre = 'Raul', edad = 25;
+
+//ES5
+var obj = {nombre:nombre,edad:edad};
+//ES6
+//Cuando la propiedad del objeto y la variable tienen el mismo
+//valor basta con poner el nombre de la propiedad del objeto
+var obj2 = {nombre,edad}; 
